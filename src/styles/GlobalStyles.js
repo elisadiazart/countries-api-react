99dd2ae8e489c8b347b6ec0599eeb20c084a7e73
Elisa <elisadiazartcontact@gmail.com>
2023-04-14 11:55:50 +0200

    14.04.23

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *,*::after,*::before{
+    box-sizing: border-box;
+    }
+    img{
+        max-width: 100%;
+        display: block;
+    }
+    ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    body {
+        margin: 0;
+        max-width: 100vw;
+        margin-left: auto;
+        margin-right: auto;
+        font-family: 'Nunito Sans', sans-serif;
+        color: black;
+        background-color: #FAFAFA
+    }
+    *{
+        margin: 0;
+    }
+`;
+
+export { GlobalStyles };
